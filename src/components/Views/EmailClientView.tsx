d348b902ab38bddce16eade68268196eb8ad33e2
Yiwei Diao <tuicaodanad@gmail.com>
2025-03-14 18:58:32 +1300

    fix style

--- src/components/Views/EmailClientView.tsx
+++ src/components/Views/EmailClientView.tsx
@@ -106,7 +106,7 @@ const EmailClientView: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit(validateAndSubmit, handleError)}
-      className="flex flex-col flex-1 font-monaco text-[12px] pr-4"
+      className="flex flex-col flex-1 font-monaco text-[12px] pr-0"
     >
       <Dialog {...dialogProps} isOpen={isOpen} onClose={closeDialog} />
       {/* Email client header */}
