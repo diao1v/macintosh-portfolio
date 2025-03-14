3eeb911d29dda034519ead15dc69cf33aa68842f
Yiwei Diao <tuicaodanad@gmail.com>
2025-03-14 18:25:04 +1300

    update font size

--- src/components/Views/EmailClientView.tsx
+++ src/components/Views/EmailClientView.tsx
@@ -106,7 +106,7 @@ const EmailClientView: React.FC = () => {
   return (
     <form
       onSubmit={handleSubmit(validateAndSubmit, handleError)}
-      className="flex flex-col flex-1 font-monaco text-[20px]"
+      className="flex flex-col flex-1 font-monaco text-[12px]"
     >
       <Dialog {...dialogProps} isOpen={isOpen} onClose={closeDialog} />
       {/* Email client header */}
