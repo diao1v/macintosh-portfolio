0ba492838ae46dbd896e8fd99305f0d83efb66d6
diao1v <tuicaodanad@gmail.com>
2026-06-04 22:58:47 +1200

    fix: update dialog icon to new info icon

--- src/config/dialogs.ts
+++ src/config/dialogs.ts
@@ -131,7 +131,7 @@ export const getDialogProps = (type: DialogType, data?: any): DialogProps => {
     dialogMap[type]?.(data) || {
       title: 'Information',
       message: 'No additional information available.',
-      icon: '/icons/about_macintosh.png',
+      icon: '/icons/info.png',
       buttons: [{ label: 'OK', onClick: () => {} }],
     }
   );
